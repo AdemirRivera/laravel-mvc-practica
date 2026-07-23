2065d0c38157b743a70bc978c15b1fae002a5da9
Ademir Rivera <uarivera@innovacion.gob.sv>
2026-07-23 14:25:26 -0600

    feat: practicas con rutas basicas y creacion de patron para ruta

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,63 @@
 
 use Illuminate\Support\Facades\Route;
 
+// -> Ruta por defecto - GET
 Route::get('/', function () {
     return 'hola mundo';
     // return view('welcome');
 });
 
+// -> Ruta contacto - GET
 // Route::get('/contacto', function(){
 //     return 'view contacto';
 // });
 
+// -> Ruta contacto - POST
 // Route::post('/contacto', function(){
 //     return 'error contacto';
 // });
 
-Route::match(['get', 'post'], '/contacto', function(){
+// -> Ruta contacto - GET y POST
+Route::match(['get', 'post'], '/contacto', function () {
     return 'view contacto';
 });
+
+// -> Ruta cursos - GET
+Route::get('/cursos/informacion', function () {
+    return 'bienvenido al curso de informatica';
+});
+
+// -> Ruta cursos con parámetro - GET
+// Route::get('/cursos/{curso}', function ($curso) {
+//     return 'bienvenido al curso ' . $curso;
+// });
+
+// -> Ruta cursos con parámetro con expresión regular - GET
+// Route::get('/cursos/{curso}', function ($curso) {
+//     return 'El curso digitado es: ' . $curso;
+// })->where('curso', '[A-Za-z]+');
+
+// -> Ruta con expresión regular mediante whereAlpha - GET
+// Route::get('/cursos/{curso}', function ($curso) {
+//     return 'El curso digitado es: ' . $curso;
+// })->whereAlpha('curso');
+
+// -> Ruta con expresión regular mediante whereIn - GET
+// Route::get('/cursos/{curso}', function ($curso) {
+//     return 'El curso digitado es: ' . $curso;
+// })->whereIn('curso', ['html', 'css', 'javascript']);
+
+Route::get('/cursos/{id}', function ($id) {
+    return 'El id digitado es: ' . $id;
+});
+
+// -> Ruta cursos con parámetro y subruta opcional - GET
+// Route::get('/cursos/{curso}/{categoria?}', function ($curso, $categoria = null) {
+//     if ($categoria) {
+//         return 'bienvenido al curso ' . $curso . ' y tu categoria es ' . $categoria;
+//     }
+//     return 'bienvenido al curso ' . $curso;
+// })->where([
+//             'curso' => '[A-Za-z]+',
+//             'categoria' => '[A-Za-z]+'
+//         ]);
